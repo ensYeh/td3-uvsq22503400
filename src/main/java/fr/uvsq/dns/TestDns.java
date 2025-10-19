2e86f99879e9a692ab29ea3c1412ac22462fcc0f
alili <aliliourdia2004@gmail.com>
2025-10-19 18:11:54 +0200

    Ajout de la classe TestDns et test du chargement du fichier dns.txt

---
+++ src/main/java/fr/uvsq/dns/TestDns.java
@@ -0,0 +1,23 @@
+package fr.uvsq.dns;
+
+public class TestDns {
+    public static void main(String[] args) {
+        try {
+            // On charge la base DNS depuis le fichier
+            Dns dns = new Dns("src/main/resources/dns.txt");
+
+            // Test 1 : recherche par nom de machine
+            NomMachine nom = new NomMachine("serveur1.uvsq.fr");
+            DnsItem item1 = dns.getItem(nom);
+            System.out.println("Résultat pour " + nom + " : " + item1);
+
+            // Test 2 : recherche par adresse IP
+            AdresseIP ip = new AdresseIP("192.168.0.2");
+            DnsItem item2 = dns.getItem(ip);
+            System.out.println("Résultat pour " + ip + " : " + item2);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
